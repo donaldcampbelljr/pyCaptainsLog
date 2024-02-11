a1bc71e0a6f96340a32044cdf52259b731a19866
donaldcampbelljr <125581724+donaldcampbelljr@users.noreply.github.com>
2024-02-11 14:46:14 -0500

    Adjusting prompts

--- sandbox_tvscript.py
+++ sandbox_tvscript.py
@@ -17,12 +17,19 @@ def main():
         model = genai.GenerativeModel('gemini-pro')
 
 
+#         input = f"""
+#         Generate 1 unique planet for a tabletop rpg. Answer in JSON format with primary keys: name, description, planet_type, items. 
+#         For items, only generate a max of 3 items. Make sure the theme is science fiction.
+# """
+
         input = f"""
-        Generate 1 unique planet for a tabletop rpg. Answer in JSON format with primary keys: name, description, planet_type, items. For items, only generate a max of 3 items. Make sure the theme is science fiction.
-"""
+        Generate 1 unique planet for a tabletop rpg. Answer in JSON format with primary keys: name, description, planet_type, items. 
+        For items, only generate a max of 3 items. Make sure the theme is science fiction Write the decription as though it is the beginning of an episode of TV.
+"""       
 
         response = model.generate_content(input,safety_settings={'HARM_CATEGORY_HARASSMENT': 'BLOCK_ONLY_HIGH'})
 
+        print("RESPONSE TEXT------------------------")
         print(response.text)
         finaltext = response.text.replace("`", "")
         finaltext = finaltext.replace("python", "")
@@ -30,12 +37,33 @@ def main():
 
         try:
             parsed_dict = json.loads(finaltext)
-
+            print("LOADED JSON------------------------")
             for k,v in parsed_dict.items():
                 print(f"{k}\n")
                 print(f"{v}\n")
         except:
             print("Could not load JSON.")
+
+        
+        if "description" in parsed_dict:
+
+            input = f"""My starship crew would like to investigate this planet more. It was the current description: 
+            {parsed_dict['description']}. Please write an episode of TV that gives the begining of this journey. 
+            Give the output in JSON format where primary keys correspond to the scene numbers. Also For example: scene_number. 
+            Do not name the characters after famous celebrities. For each scene also generate a science fiction themed item that would be used in a tabletop card game."""
+
+            response = model.generate_content(input,safety_settings={'HARM_CATEGORY_HARASSMENT': 'BLOCK_ONLY_HIGH'})
+
+            print("RESPONSE TEXT------------------------")
+            print(response.text)
+            finaltext = response.text.replace("`", "")
+            finaltext = finaltext.replace("python", "")
+            finaltext = finaltext.replace("json", "")
+            print("LOADED JSON------------------------")
+            parsed_dict = json.loads(finaltext)
+            for k,v in parsed_dict.items():
+                print(f"{k}\n")
+                print(f"{v}\n")
 
 
 
