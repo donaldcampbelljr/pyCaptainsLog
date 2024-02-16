b5cd38e3a2271661af91f258078905aef696273f
donaldcampbelljr <125581724+donaldcampbelljr@users.noreply.github.com>
2024-02-16 07:09:28 -0500

    add planet types, anomalies

--- constants.py
+++ constants.py
@@ -1,9 +1,28 @@
 
 STAR_DIRECTORY = "starsystems"
+
 PLANET_NOUNS =  ['planet', 'p', 'pl']
 LEAVE_COMMANDS = ["l", "le", "leave"]
 GET_COMMANDS = ["g", "get", "GET", "ge"]
+SCAN_COMMANDS = ["sc", "scan"]
+
 DIPLOMACY = "diplomacy"
 STRENGTH = "strength"
 SCIENCE = "science"
 
+PLANET_TYPES = ["M-Class", "Desert", "Swamp", "Tundra", "Gas Giant", "Dwarf", "Dyson Sphere"]
+
+CARD_TYPES = [DIPLOMACY, STRENGTH, SCIENCE]
+
+
+ANOMALIES = [
+
+"Wormholes",
+"Black Holes",
+"Dark Matter Singularity",
+"Quantum Foam",
+"Distress Signal",
+"Ancient Probe",
+
+
+]
