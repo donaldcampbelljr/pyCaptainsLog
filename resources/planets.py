914051c0336bed1683b0179a76958383ea458fe6
“donaldcampbelljr” <125581724+donaldcampbelljr@users.noreply.github.com>
2024-06-05 06:41:16 -0400

    add some pre-generated planets, systems, and item descriptions

---
+++ resources/planets.py
@@ -0,0 +1,24 @@
+planet_descriptions = [
+
+"Potentially habitable super-Earth",
+"Tidally locked rocky exoplanet",
+"Earth-sized within habitable zone",
+"Prime Earth-sized candidate",
+"Rocky planet near Proxima Centauri",
+"Scorching hot Jupiter",
+"Potential hot super-Earth ocean world",
+"Hot Jupiter with water vapor atmosphere",
+"Potentially rocky super-Earth in habitable zone",
+"Rocky super-Earth candidate near Sun-like star",
+"Tidally locked hot super-Earth",
+"Ultra-hot Jupiter exceeding 2,000 °C",
+"Possibly rocky Earth-sized in habitable zone",
+"Sun-like star's habitable zone Earth-size candidate",
+"Tidally locked rocky super-Earth in habitable zone"
+]
+
+
+special_planets = {
+    "Earth 2": "A mirror Earth found in some distant star system",
+    "Vulkan": "A planet with pointy-eared humanoids",
+}
