6d8d862e177b07afa4c6d6e007013ac980a46ea0
donaldcampbelljr <125581724+donaldcampbelljr@users.noreply.github.com>
2024-06-25 17:51:20 -0400

    add cards

--- sqlmodeltest.py
+++ sqlmodeltest.py
@@ -7,7 +7,8 @@ from typing import Optional
 class StarSystem(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
-    event_list: Optional[list] = Field(None, sa_type=JSON)
+    forward_unlocked: bool = False
+    #event_list: Optional[list] = Field(None, sa_type=JSON)
     # eventlist: list = Field(sa_column=Column(JSON),
     #                     default=null(),)
     # systemdict: Optional[dict] = Field(sa_column=Column(JSON),
@@ -18,6 +19,7 @@ class StoryEvent(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
     intro_text: str
+    health: int
     strength: int
     event_type: str
 
@@ -25,9 +27,15 @@ class Planet(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
     intro_text: str
-    event_list: Optional[list] = Field(None, sa_type=JSON)
+    event_id: str #story event id associated with the planet
+    #event_list: Optional[list] = Field(None, sa_type=JSON)
 
-
+class Card(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    intro_text: str
+    strength: int
+    card_type: str
 
 def main():
 
@@ -39,8 +47,9 @@ def main():
 
     add_star_system(engine)
 
-    retrieve_star_system(name = "Urth System", engine = engine)
+    star_system = retrieve_star_system(name = "Urth System", engine = engine)
 
+    print(f"Here is retrieved the star system {star_system.name}")
 
 
     return 0
@@ -49,9 +58,11 @@ def retrieve_star_system(name, engine):
 
      with Session(engine) as session:
         statement = select(StarSystem).where(StarSystem.name == name)
-        results = session.exec(statement).all()
-        for system in results:
-            print(system)   
+        results = session.exec(statement).first()
+        # type(results.event_list)
+        # for system in results:
+        #     print(system)
+        return results   
 
 
 def add_star_system(engine):
@@ -60,9 +71,10 @@ def add_star_system(engine):
     random_dict = {'a':1, 'b': 2}
     systemlist = [1,2,3,4,5,6,7]
 
-    system1 = StarSystem(name="Urth System", event_list=systemlist)
-    event1 = StoryEvent(name="UrthEvent", intro_text="Here is some intro text for the Urth Event.", strength=5, event_type="combat")
-    planet1 = Planet(name="Urth", intro_text="Some intro text for planet Urth")
+    system1 = StarSystem(name="Urth System", forward_unlocked=True)
+    event1 = StoryEvent(name="UrthEvent", intro_text="Here is some intro text for the Urth Event.", strength=5, health=9,event_type="combat")
+    planet1 = Planet(name="Urth", intro_text="Some intro text for planet Urth", event_id="random-event_identifier")
+    card1 = Card(name="card1", intro_text="card1 intro text", strength=6, card_type="science")
 
     with Session(engine) as session:
 
@@ -71,6 +83,8 @@ def add_star_system(engine):
         session.add(system1)
 
         session.add(planet1)
+        
+        session.add(card1)
         
         session.commit()
         
